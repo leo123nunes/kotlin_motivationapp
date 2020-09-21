3d822d7b79e85666f542fe518911304f1a60c4b0
leo123nunes <ti.userleo@hotmail.com>
2020-09-21 10:04:29 -0300

    Criando projeto

---
+++ app/src/main/java/com/leo123nunes/motivationapp/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.leo123nunes.motivationapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
