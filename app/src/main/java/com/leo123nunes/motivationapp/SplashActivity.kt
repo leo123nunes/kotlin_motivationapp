aed3fa478c6ba9e98bebe951cceb61ad94d5689d
leo123nunes <ti.userleo@hotmail.com>
2020-09-21 11:25:00 -0300

    Adicionando evento ao clicar no botão Save da tela inicial

--- app/src/main/java/com/leo123nunes/motivationapp/SplashActivity.kt
+++ app/src/main/java/com/leo123nunes/motivationapp/SplashActivity.kt
@@ -1,15 +1,29 @@
 package com.leo123nunes.motivationapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_splash.*
 
-class SplashActivity : AppCompatActivity() {
+class SplashActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
         if(supportActionBar !=null){
             supportActionBar!!.hide()
+        }
+
+        buttonSave.setOnClickListener(this)
+    }
+
+    override fun onClick(view: View) {
+        if(userName.text.toString()==""){
+            Toast.makeText(this,"Por gentileza, digite seu nome.",Toast.LENGTH_LONG).show()
+        }else{
+            startActivity(Intent(this,MainActivity::class.java))
         }
     }
 }
